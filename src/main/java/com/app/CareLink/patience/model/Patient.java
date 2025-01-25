453952ae4b790e376109e82333a11e81b79bc59f
Jacob Oluwafemi <jacoboluwafemi2002@gmail.com>
2025-01-25 12:09:18 +0100

    Database implementation

--- src/main/java/com/app/CareLink/patience/model/Patient.java
+++ src/main/java/com/app/CareLink/patience/model/Patient.java
@@ -1,7 +1,9 @@
 package com.app.CareLink.patience.model;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.Data;
 import org.springframework.stereotype.Component;
 
@@ -10,16 +12,26 @@ import java.time.LocalDate;
 @Component
 @Entity
 @Data
+@Table(name = "patient")
 public class Patient {
+    @Column(name = "firstname", nullable = false)
     private String firstName;
+    @Column(name = "lastname")
     private String lastName;
+    @Column(name = "phoneNumber")
     private String phoneNumber;
+    @Column(name = "email")
     private String email;
+    @Column(name = "address")
     private String address;
+    @Column(name = "gender")
     private String gender;
+    @Column(name = "dateOfBirth")
     private LocalDate birthDate;
     @Id
+    @Column(name = "username")
     private String username;
+    @Column(name = "password")
     private String password;
 
 }
