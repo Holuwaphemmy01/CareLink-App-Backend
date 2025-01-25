0db872b1e6aba69d14a9b2c741b8286492730a33
Jacob Oluwafemi <jacoboluwafemi2002@gmail.com>
2025-01-25 02:42:52 +0100

    first commit

---
+++ src/test/java/com/app/CareLink/CareLinkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.CareLink;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CareLinkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
